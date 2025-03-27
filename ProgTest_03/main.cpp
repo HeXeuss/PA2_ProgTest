e7e9dd114f9ee3e06ef35246ee888116c69be60d
HeXeuss <pato.hrasko@gmail.com>
2025-03-27 19:15:42 +0100

    Progtest_03

--- ProgTest_03/main.cpp
+++ ProgTest_03/main.cpp
@@ -7,58 +7,315 @@
 #include <sstream>
 #endif /* __PROGTEST__ */
 
-class CRegister {
+class String {
 public:
-    // Default constructor: Initializes an empty registry.
-    CRegister();
 
-    // Copy constructor: Creates a deep copy of another instance.
-    CRegister(const CRegister &other);
+    explicit String(const char* str = ""){
+        if (!str) str = "";
+        _len = std::strlen(str);
+        _data = new char[_len + 1];
+        std::memcpy(_data, str, _len + 1);
+    }
+    String(const String& other){
+        _len = other._len;
+        _data = new char[_len + 1];
+        std::memcpy(_data,other._data, _len + 1);
+    }
+    ~String(){
+        delete[] _data;
+    }
 
-    // Destructor: Cleans up allocated resources.
-    ~CRegister();
+    String& operator=(String other){
+        swap(other);
+        return *this;
+    }
 
-    // Assignment operator: Performs deep copy to avoid memory leaks.
-    CRegister &operator=(const CRegister &other);
+    String& operator+=(const String& other){
+        size_t newLen = _len + other._len;
+        char* newData = new char[newLen + 1];
+        std::memcpy(newData, _data, _len);
+        std::memcpy(newData + _len, other._data, other._len + 1);
+        delete[] _data;
+        _data = newData;
+        _len = newLen;
+        return *this;
+    }
+    friend String operator+(const String& a, const String & b){
+        String newStr(a);
+        newStr += b;
+        return newStr;
+    }
 
-    /**
-     * Adds a new person to the registry.
-     * @param id Unique identifier for the person (format "XXXXXX/YYYY").
-     * @param name First name of the person (arbitrary length).
-     * @param surname Last name of the person (arbitrary length).
-     * @param date Date of residence validity start (format "YYYY-MM-DD").
-     * @param street Street address (arbitrary length and content).
-     * @param city City of residence (arbitrary length and content).
-     * @return True if successfully added, False if an entry with the same ID already exists.
-     */
-    bool add(const char id[], const char name[], const char surname[],
-             const char date[], const char street[], const char city[]);
+    bool operator<(const String & other) const {
+        return std::strcmp(_data, other._data) < 0;
+    }
+    bool operator>(const String & other) const {
+        return std::strcmp(_data, other._data) > 0;
+    }
+    bool operator<=(const String & other) const {
+        return std::strcmp(_data, other._data) <= 0;
+    }
+    bool operator>=(const String & other) const{
+        return std::strcmp(_data, other._data) >= 0;
+    }
+    bool operator==(const String & other) const {
+        return std::strcmp(_data, other._data) == 0;
+    }
+    bool operator!=(const String & other) const{
+        return std::strcmp(_data, other._data) != 0;
+    }
+    char operator[](const size_t index) const {
+        return _data[index];
+    }
+    char &operator[](const size_t index){
+        return _data[index];
+    }
 
-    /**
-     * Updates an existing person's address in the registry.
-     * @param id Unique identifier of the person.
-     * @param date Date of residence change (format "YYYY-MM-DD").
-     * @param street New street address.
-     * @param city New city.
-     * @return True if successfully updated, False if the ID does not exist
-     *         or if an address change already exists for that date.
-     */
-    bool resettle(const char id[], const char date[], const char street[], const char city[]);
+    friend std::ostream & operator<<(std::ostream & os, const String & str){
+        os << str._data;
+        return  os;
+    }
 
-    /**
-     * Prints the record of a person with the given ID.
-     * @param os Output stream to print to.
-     * @param id Unique identifier of the person.
-     * @return True if the person exists and was printed, False otherwise.
-     * @note Addresses are printed in ascending order of the change date.
-     */
-    bool print(std::ostream &os, const char id[]) const;
+    void swap(String &other) noexcept {
+        std::swap(_data, other._data);
+        std::swap(_len,  other._len);
+    }
 
+    size_t length() const {
+        return _len;
+    }
 private:
-    // Internal data structure to store records.
-    // TODO: Define appropriate storage for the registry.
+    char * _data;
+    size_t _len;
 };
 
+template <typename T>
+class Array {
+public:
+    Array() : _size(0), _capacity(0), _data(nullptr) {}
+    Array(const Array &other) : _size(other._size), _capacity(other._capacity), _data(new T[other._capacity]) {
+        for (size_t i = 0; i < other._size; i++)
+            _data[i] = other._data[i];
+    }
+    ~Array() {
+        delete[] _data;
+    }
+
+    void swap(Array &other) noexcept {
+        std::swap(_size, other._size);
+        std::swap(_capacity, other._capacity);
+        std::swap(_data, other._data);
+    }
+
+    Array &operator=(Array other) {
+        swap(other);
+        return *this;
+    }
+
+    T &operator[](size_t index) {
+        return _data[index];
+    }
+
+    const T &operator[](size_t index) const {
+        return _data[index];
+    }
+
+    void push_back(const T &value) {
+        if (_size >= _capacity) {
+            _capacity = _capacity * 2 + 1;
+            T *new_data = new T[_capacity];
+            for (size_t i = 0; i < _size; i++) {
+                new_data[i] = _data[i];
+            }
+            delete[] _data;
+            _data = new_data;
+        }
+        _data[_size++] = value;
+    }
+
+    void erase(const size_t index) {
+        if (index >= _size) return;
+        for (size_t i = index; i < _size - 1; i++) {
+            _data[i] = std::move(_data[i + 1]);
+        }
+        _size--;
+    }
+
+    size_t size() const {
+        return _size;
+    }
+
+    size_t end() const {
+        return _size;
+    }
+
+    size_t find(const T &value) const {
+        for (size_t i = 0; i < _size; i++) {
+            if (_data[i] == value) return i;
+        }
+        return end();
+    }
+
+    void print() const {
+        for (size_t i = 0; i < _size; i++) std::cout << _data[i] << " ";
+        std::cout << std::endl;
+    }
+
+    void sort() {
+        for (size_t i = 0; i < _size - 1; i++) {
+            for (size_t j = 0; j < _size - 1 - i; j++) {
+                if (_data[j + 1] < _data[j]) {
+                    std::swap(_data[j], _data[j + 1]);
+                }
+            }
+        }
+    }
+
+private:
+    size_t _size;
+    size_t _capacity;
+    T *_data;
+};
+
+class Address {
+    public:
+    explicit Address(const String& date = String(""), const String& street = String(""), const String& city = String(""))
+    : _date(date), _street(street), _city(city) {}
+
+    Address(const Address& other)
+        : _date(other._date), _street(other._street), _city(other._city) {}
+
+    Address& operator=(Address other) {
+        swap(other);
+        return *this;
+    }
+
+    void swap(Address &other) noexcept {
+        std::swap(_date, other._date);
+        std::swap(_street, other._street);
+        std::swap(_city, other._city);
+    }
+
+    bool operator==(const Address& other) const {
+        return _date == other._date;
+    }
+
+    bool operator<(const Address& other) const {
+        return _date < other._date;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Address& address) {
+        os << address._date << " " << address._street << " " << address._city;
+        return os;
+    }
+private:
+    String _date;
+    String _street;
+    String _city;
+};
+
+class Person {
+public:
+    explicit Person(const String& id = String(""), const String& name = String(""), const String& surname = String(""))
+        : _id(id), _name(name), _surname(surname) {}
+
+    Person(const Person& other)
+        : _id(other._id), _name(other._name), _surname(other._surname), _addresses(other._addresses) {}
+
+    Person& operator=(Person other) {
+        swap(other);
+        return *this;
+    }
+
+    void swap(Person &other) noexcept {
+        std::swap(_id, other._id);
+        std::swap(_name, other._name);
+        std::swap(_surname, other._surname);
+        std::swap(_addresses, other._addresses);
+    }
+
+    bool operator==(const Person& other) const {
+        return _id == other._id;
+    }
+
+    bool operator!=(const Person& other) const {
+        return !(*this == other);
+    }
+
+    bool settle(const Address& address) {
+        for (size_t i = 0; i < _addresses.size(); i++) {
+            if (_addresses[i] == address) {
+                return false;
+            }
+        }
+        _addresses.push_back(address);
+        _addresses.sort();
+        return true;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Person& person) {
+        os << person._id << " " << person._name << " " << person._surname << std::endl;
+        for (size_t i = 0; i < person._addresses.size(); ++i) {
+            os << "  " << person._addresses[i] << std::endl;
+        }
+        os << "  ";
+        return os;
+    }
+
+private:
+    String _id;
+    String _name;
+    String _surname;
+    Array<Address> _addresses;
+};
+
+class CRegister {
+    Array<Person> _people;
+public:
+    CRegister() {
+        _people = Array<Person>();
+    }
+
+    CRegister(const CRegister &other) {
+        _people = other._people;
+    }
+
+    ~CRegister() = default;
+
+    CRegister &operator=(CRegister other) {
+        swap(other);
+        return *this;
+    }
+
+    void swap(CRegister &other) noexcept {
+        std::swap(_people, other._people);
+    }
+
+    bool add(const char id[], const char name[], const char surname[],
+             const char date[], const char street[], const char city[]) {
+        Person newPerson = Person(String(id), String(name),String(surname));
+        size_t it = _people.find(newPerson);
+        if (it != _people.end()) return false;
+        newPerson.settle(Address(String(date), String(street), String(city)));
+        _people.push_back(newPerson);
+
+        return true;
+    }
+
+    bool resettle(const char id[], const char date[], const char street[], const char city[]) {
+        size_t it_id = _people.find(Person(String(id), String(""), String("")));
+        if (it_id == _people.end()) return false;
+        Address newAddress = Address(String(date), String(street), String(city));
+        return _people[it_id].settle(newAddress);
+    }
+
+    bool print(std::ostream &os, const char id[]) const {
+        size_t it_id = _people.find(Person(String(id), String(""), String("")));
+        if (it_id == _people.end()) return false;
+        os << _people[it_id];
+        return true;
+    }
+};
 
 #ifndef __PROGTEST__
 int main ()
@@ -97,8 +354,8 @@ int main ()
   2001-02-03 Elm street Sacramento
   2008-04-12 Elm street Cinccinati
   )###" ) );
-    a = b;
-    assert ( a . resettle ( "987654/3210", "2011-05-05", "Elm street", "Salt Lake City" ) == true );
+  a = b;
+  assert ( a . resettle ( "987654/3210", "2011-05-05", "Elm street", "Salt Lake City" ) == true );
     oss . str ( "" );
     assert ( a . print ( oss, "987654/3210" ) == true );
     assert ( ! strcmp ( oss . str () . c_str (), R"###(987654/3210 Freddy Kruger

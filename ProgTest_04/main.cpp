090ce737093160c5f8ddab2acc7c0248a6a7244a
HeXeuss <pato.hrasko@gmail.com>
2025-03-30 13:18:48 +0200

    ProgTest_04

---
+++ ProgTest_04/main.cpp
@@ -0,0 +1,128 @@
+#ifndef __PROGTEST__
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <memory>
+#include <compare>
+#include <functional>
+#include <optional>
+
+class CTimeStamp
+{
+  public:
+                          CTimeStamp    ( int               year,
+                                          int               month,
+                                          int               day,
+                                          int               hour,
+                                          int               minute,
+                                          double            sec );
+    int                   compare       ( const CTimeStamp & x ) const;
+    friend std::ostream & operator <<   ( std::ostream     & os,
+                                          const CTimeStamp & x );
+  private:
+};
+class CMail
+{
+  public:
+                          CMail         ( const CTimeStamp & timeStamp,
+                                          const std::string & from,
+                                          const std::string & to,
+                                          const std::optional<std::string> & subject );
+    int                   compareByTime ( const CTimeStamp & x ) const;
+    int                   compareByTime ( const CMail      & x ) const;
+    const std::string   & from          () const;
+    const std::string   & to            () const;
+    //const std::optional<std::string>  & subject       () const;
+    const CTimeStamp    & timeStamp     () const;
+    friend std::ostream & operator <<   ( std::ostream     & os,
+                                          const CMail      & x );
+  private:
+};
+// your code will be compiled in a separate namespace
+namespace MysteriousNamespace {
+#endif /* __PROGTEST__ */
+//----------------------------------------------------------------------------------------
+class CMailLog
+{
+  public:
+    int                   parseLog      ( std::istream     & in );
+
+    std::list<CMail>      listMail      ( const CTimeStamp & from,
+                                          const CTimeStamp & to ) const;
+
+    std::set<std::string> activeUsers   ( const CTimeStamp & from,
+                                          const CTimeStamp & to ) const;
+  private:
+    // todo
+};
+//----------------------------------------------------------------------------------------
+#ifndef __PROGTEST__
+} // namespace
+std::string             printMail       ( const std::list<CMail> & all )
+{
+  std::ostringstream oss;
+  for ( const auto & mail : all )
+    oss << mail << "\n";
+  return oss . str ();
+}
+int                     main ()
+{
+  MysteriousNamespace::CMailLog m;
+  std::list<CMail> mailList;
+  std::set<std::string> users;
+  std::istringstream iss;
+
+  iss . clear ();
+  iss . str (
+    "Mar 29 2025 12:35:32.233 relay.fit.cvut.cz ADFger72343D: from=user1@fit.cvut.cz\n"
+    "Mar 29 2025 12:37:16.234 relay.fit.cvut.cz JlMSRW4232Df: from=person3@fit.cvut.cz\n"
+    "Mar 29 2025 12:55:13.023 relay.fit.cvut.cz JlMSRW4232Df: subject=New progtest homework!\n"
+    "Mar 29 2025 13:38:45.043 relay.fit.cvut.cz Kbced342sdgA: from=office13@fit.cvut.cz\n"
+    "Mar 29 2025 13:36:13.023 relay.fit.cvut.cz JlMSRW4232Df: to=user76@fit.cvut.cz\n"
+    "Mar 29 2025 13:55:31.456 relay.fit.cvut.cz KhdfEjkl247D: from=PR-department@fit.cvut.cz\n"
+    "Mar 29 2025 14:18:12.654 relay.fit.cvut.cz Kbced342sdgA: to=boss13@fit.cvut.cz\n"
+    "Mar 29 2025 14:48:32.563 relay.fit.cvut.cz KhdfEjkl247D: subject=Business partner\n"
+    "Mar 29 2025 14:58:32.000 relay.fit.cvut.cz KhdfEjkl247D: to=HR-department@fit.cvut.cz\n"
+    "Mar 29 2025 14:25:23.233 relay.fit.cvut.cz ADFger72343D: mail undeliverable\n"
+    "Mar 29 2025 15:02:34.231 relay.fit.cvut.cz KhdfEjkl247D: to=CIO@fit.cvut.cz\n"
+    "Mar 29 2025 15:02:34.230 relay.fit.cvut.cz KhdfEjkl247D: to=CEO@fit.cvut.cz\n"
+    "Mar 29 2025 15:02:34.230 relay.fit.cvut.cz KhdfEjkl247D: to=dean@fit.cvut.cz\n"
+    "Mar 29 2025 15:02:34.230 relay.fit.cvut.cz KhdfEjkl247D: to=vice-dean@fit.cvut.cz\n"
+    "Mar 29 2025 15:02:34.230 relay.fit.cvut.cz KhdfEjkl247D: to=archive@fit.cvut.cz\n" );
+  assert ( m . parseLog ( iss ) == 8 );
+  mailList = m . listMail ( CTimeStamp ( 2025, 3, 28, 0, 0, 0 ),
+                            CTimeStamp ( 2025, 3, 29, 23, 59, 59 ) );
+  assert ( printMail ( mailList ) == R"###(2025-03-29 13:36:13.023 person3@fit.cvut.cz -> user76@fit.cvut.cz, subject: New progtest homework!
+2025-03-29 14:18:12.654 office13@fit.cvut.cz -> boss13@fit.cvut.cz
+2025-03-29 14:58:32.000 PR-department@fit.cvut.cz -> HR-department@fit.cvut.cz, subject: Business partner
+2025-03-29 15:02:34.230 PR-department@fit.cvut.cz -> CEO@fit.cvut.cz, subject: Business partner
+2025-03-29 15:02:34.230 PR-department@fit.cvut.cz -> dean@fit.cvut.cz, subject: Business partner
+2025-03-29 15:02:34.230 PR-department@fit.cvut.cz -> vice-dean@fit.cvut.cz, subject: Business partner
+2025-03-29 15:02:34.230 PR-department@fit.cvut.cz -> archive@fit.cvut.cz, subject: Business partner
+2025-03-29 15:02:34.231 PR-department@fit.cvut.cz -> CIO@fit.cvut.cz, subject: Business partner
+)###" );
+  mailList = m . listMail ( CTimeStamp ( 2025, 3, 28, 0, 0, 0 ),
+                            CTimeStamp ( 2025, 3, 29, 14, 58, 32 ) );
+  assert ( printMail ( mailList ) == R"###(2025-03-29 13:36:13.023 person3@fit.cvut.cz -> user76@fit.cvut.cz, subject: New progtest homework!
+2025-03-29 14:18:12.654 office13@fit.cvut.cz -> boss13@fit.cvut.cz
+2025-03-29 14:58:32.000 PR-department@fit.cvut.cz -> HR-department@fit.cvut.cz, subject: Business partner
+)###" );
+  mailList = m . listMail ( CTimeStamp ( 2025, 3, 30, 0, 0, 0 ),
+                            CTimeStamp ( 2025, 3, 30, 23, 59, 59 ) );
+  assert ( printMail ( mailList ) == "" );
+  users = m . activeUsers ( CTimeStamp ( 2025, 3, 28, 0, 0, 0 ),
+                            CTimeStamp ( 2025, 3, 29, 23, 59, 59 ) );
+  assert ( users == std::set<std::string>( { "CEO@fit.cvut.cz", "CIO@fit.cvut.cz", "HR-department@fit.cvut.cz", "PR-department@fit.cvut.cz", "archive@fit.cvut.cz", "boss13@fit.cvut.cz", "dean@fit.cvut.cz", "office13@fit.cvut.cz", "person3@fit.cvut.cz", "user76@fit.cvut.cz", "vice-dean@fit.cvut.cz" } ) );
+  users = m . activeUsers ( CTimeStamp ( 2025, 3, 28, 0, 0, 0 ),
+                            CTimeStamp ( 2025, 3, 29, 13, 59, 59 ) );
+  assert ( users == std::set<std::string>( { "person3@fit.cvut.cz", "user76@fit.cvut.cz" } ) );
+  return EXIT_SUCCESS;
+}
+#endif /* __PROGTEST__ */
